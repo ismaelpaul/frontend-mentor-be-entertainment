f80383a5da37c9ced038c8431d16340818c0a4ce
ismaelpaul <ismaafp@gmail.com>
2023-02-25 21:37:44 +0000

    Add server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+const express = require('express');
+const apiRouter = require('./routes/api.router');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Route
+app.use('/api', apiRouter);
+
+module.exports = app;
