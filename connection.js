4952210de73bc432f18e14071ab39028c951d8f8
ismaelpaul <ismaafp@gmail.com>
2023-02-25 21:38:11 +0000

    Add connection to db

---
+++ connection.js
@@ -0,0 +1,18 @@
+const app = require('./server');
+const mongoose = require('mongoose');
+
+const { PORT = 9090 } = process.env;
+
+//Connect DB and start server
+mongoose
+	.set('strictQuery', true)
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		app.listen(PORT, (err) => {
+			if (err) throw err;
+			console.log(`Listening on port ${PORT}...`);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
