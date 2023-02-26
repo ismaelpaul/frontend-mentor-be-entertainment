5992faaf9132199b6ba127f9f00bcf50e4dab2c3
ismaelpaul <ismaafp@gmail.com>
2023-02-26 18:59:36 +0000

    Add bookmark controller

---
+++ controllers/bookmark.controllers.js
@@ -0,0 +1,46 @@
+const asyncHandler = require('express-async-handler');
+const Bookmark = require('../models/bookmark.models');
+
+exports.getBookmarks = asyncHandler(async (req, res) => {
+	const bookmarks = await Bookmark.find({ user: req.user.id }).sort(
+		'-createdAt'
+	);
+	res.status(200).json(bookmarks);
+});
+
+exports.addBookmark = asyncHandler(async (req, res) => {
+	const { title, thumbnail, year, category, rating } = req.body;
+
+	if (!title || !thumbnail || !year || !category || !rating) {
+		res.status(400);
+		throw new Error('Some field is missing');
+	}
+
+	const bookmark = await Bookmark.create({
+		user: req.user.id,
+		title,
+		thumbnail,
+		year,
+		category,
+		rating,
+	});
+
+	res.status(201).json(bookmark);
+});
+
+exports.deleteBookmark = asyncHandler(async (req, res) => {
+	const singleBookmark = await Bookmark.findById(req.params.id);
+	const { id } = req.params;
+
+	if (!singleBookmark) {
+		res.status(400);
+		throw new Error('Bookmark not found');
+	}
+
+	if (singleBookmark.user.toString() !== req.user.id) {
+		res.status(401);
+		throw new Error('User not authorized');
+	}
+	await Bookmark.deleteOne({ _id: id });
+	res.status(200).json({ message: 'Bookmark removed successfully' });
+});
