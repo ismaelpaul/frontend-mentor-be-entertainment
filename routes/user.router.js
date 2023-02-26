0618cc8c3708384c7f28e324d61290fdaa190071
ismaelpaul <ismaafp@gmail.com>
2023-02-26 19:03:05 +0000

    Add /login, /logout and /loggedin routes

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { registerUser } = require('../controllers/user.controllers');
+const {
+	registerUser,
+	loginUser,
+	logoutUser,
+	loggedinUser,
+} = require('../controllers/user.controllers');
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
+userRouter.post('/login', loginUser);
+userRouter.get('/logout', logoutUser);
+userRouter.get('/loggedin', loggedinUser);
 
 module.exports = userRouter;
