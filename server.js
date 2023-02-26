8f64dde2e359df3277f1b31e9ee084ee73b0a456
ismaelpaul <ismaafp@gmail.com>
2023-02-26 19:03:47 +0000

    Add cookieParser and error handling

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const apiRouter = require('./routes/api.router');
 const dotenv = require('dotenv').config();
+const errorHandler = require('./middleware/error.middleware');
+const apiRouter = require('./routes/api.router');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 // Route
 app.use('/api', apiRouter);
+
+// Error
+app.use('/*', (req, res) => {
+	res.status(404).send({ msg: 'Page not found.' });
+});
+
+app.use(errorHandler);
 
 module.exports = app;
