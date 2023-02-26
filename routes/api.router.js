20b26cb63fb8b0bcece4c98b47bf669df8042532
ismaelpaul <ismaafp@gmail.com>
2023-02-26 19:04:30 +0000

    Add /bookmarks route

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { getApi } = require('../controllers/api.controllers');
+const bookmarkRouter = require('./bookmark.router');
 const userRouter = require('./user.router');
 
 const apiRouter = express.Router();
@@ -9,5 +10,8 @@ apiRouter.get('/', getApi);
 
 // /user
 apiRouter.use('/user', userRouter);
+
+// /bookmarks
+apiRouter.use('/bookmarks', bookmarkRouter);
 
 module.exports = apiRouter;
