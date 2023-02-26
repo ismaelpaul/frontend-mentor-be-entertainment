dbc0fd0e3068dbb022666bf11b537334c2cfb225
ismaelpaul <ismaafp@gmail.com>
2023-02-26 19:02:01 +0000

    Add loginUser, logoutUser and loggedinUser controller functions

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user.models');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const generateToken = (id) => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
@@ -19,6 +20,7 @@ exports.registerUser = asyncHandler(async (req, res) => {
 		throw new Error('Password must have at least 8 characters');
 	}
 
+	//Check if user exists
 	const userExists = await User.findOne({ email });
 
 	if (userExists) {
@@ -27,7 +29,7 @@ exports.registerUser = asyncHandler(async (req, res) => {
 	}
 
 	// Create new user
-	const user = await User.create({
+	const newUser = await User.create({
 		email,
 		password,
 	});
@@ -44,8 +46,8 @@ exports.registerUser = asyncHandler(async (req, res) => {
 		secure: true,
 	});
 
-	if (user) {
-		const { _id, email, picture } = user;
+	if (newUser) {
+		const { _id, email, picture } = newUser;
 		res.status(201).json({
 			_id,
 			email,
@@ -56,4 +58,73 @@ exports.registerUser = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error('Invalid user data');
 	}
+});
+
+exports.loginUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		res.status(400);
+		throw new Error("Can't be empty");
+	}
+
+	//Check if user exists
+	const userExists = await User.findOne({ email });
+
+	if (!userExists) {
+		res.status(400);
+		throw new Error('User not found, please sign up');
+	}
+
+	//User exists, check if password is correct
+	const passwordIsCorrect = await bcrypt.compare(password, userExists.password);
+
+	// Generate token
+	const token = generateToken(userExists._id);
+
+	// Send HTTP-only cookie
+	if (passwordIsCorrect) {
+		res.cookie('token', token, {
+			path: '/',
+			httpOnly: true,
+			expires: new Date(Date.now() + 1000 * 86400), //1 day,
+			sameSite: 'none',
+			secure: true,
+		});
+	}
+
+	if (userExists && passwordIsCorrect) {
+		const { _id, email, picture } = userExists;
+
+		res.status(200).json({ _id, email, picture, token });
+	} else {
+		res.status(400);
+		throw new Error('Invalid email or password');
+	}
+});
+
+exports.logoutUser = asyncHandler(async (req, res) => {
+	res.cookie('token', '', {
+		path: '/',
+		httpOnly: true,
+		expires: new Date(0), // expires cookie
+		sameSite: 'none',
+		secure: true,
+	});
+	return res.status(200).json({ message: 'Logged out successfully' });
+});
+
+exports.loggedinUser = asyncHandler(async (req, res) => {
+	const token = req.cookies.token;
+
+	if (!token) {
+		return res.json(false);
+	}
+
+	const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+	if (verified) {
+		return res.json(true);
+	}
+	return res.json(false);
 });
