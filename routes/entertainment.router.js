e048a580c13a13990479ea905b2a74ca5eb7b8b6
ismaelpaul <ismaafp@gmail.com>
2023-03-22 17:41:38 +0000

    Add getSingleEntertainment router

--- routes/entertainment.router.js
+++ routes/entertainment.router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const {
 	getAllEntertainments,
+	getSingleEntertainment,
 } = require('../controllers/entertainment.controllers');
+const { protect } = require('../middleware/auth.middleware');
 
 const entertainmentRouter = express.Router();
 
-entertainmentRouter.get('/', getAllEntertainments);
+entertainmentRouter.get('/', protect, getAllEntertainments);
+entertainmentRouter.get('/:id', protect, getSingleEntertainment);
 
 module.exports = entertainmentRouter;
