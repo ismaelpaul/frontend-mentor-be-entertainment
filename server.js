0c9142256b4e2d9916fbaf2f405db98ab6d83e5f
ismaelpaul <ismaafp@gmail.com>
2023-03-22 17:39:49 +0000

    Add whiteList and corsOptions function

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const errorHandler = require('./middleware/error.middleware');
 const apiRouter = require('./routes/api.router');
 const cookieParser = require('cookie-parser');
 
 const app = express();
 
+const whitelist = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	credentials: true,
+};
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 
 // Route
 app.use('/api', apiRouter);
