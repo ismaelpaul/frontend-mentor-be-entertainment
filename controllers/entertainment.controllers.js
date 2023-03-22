71761612ee6808884f250867f5832a17c2d94b1f
ismaelpaul <ismaafp@gmail.com>
2023-03-22 17:40:22 +0000

    Add getSingleEntertainment function

--- controllers/entertainment.controllers.js
+++ controllers/entertainment.controllers.js
@@ -5,3 +5,14 @@ exports.getAllEntertainments = asyncHandler(async (req, res) => {
 	const entertainments = await Entertainment.find();
 	res.status(200).json(entertainments);
 });
+
+exports.getSingleEntertainment = asyncHandler(async (req, res) => {
+	const singleEntertainment = await Entertainment.findById(req.params.id);
+
+	if (!singleEntertainment) {
+		res.status(404);
+		throw new Error('Show not found');
+	}
+
+	res.status(200).json(singleEntertainment);
+});
