cca1d79c804bc4e9f8c0cda343900e6b8e0301c3
ismaelpaul <ismaafp@gmail.com>
2023-03-24 09:03:42 +0000

    The first parameter to `mongoose.connect()` must be a string

--- connection.js
+++ connection.js
@@ -5,8 +5,8 @@ const { PORT = 9090 } = process.env;
 
 //Connect DB and start server
 mongoose
-	.set('strictQuery', true)
 	.connect(process.env.MONGO_URI)
+	.set('strictQuery', true)
 	.then(() => {
 		app.listen(PORT, (err) => {
 			if (err) throw err;
